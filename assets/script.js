4665add28e8a813df6a55bec524bda69d554979a
Chris Gutierrez <cmgson1@gmail.com>
2021-01-22 22:55:39 -0600

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+$api = '9ec8fa29518e4540182f9fb78ea5599d'
+$call = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+
+oneCallapi ='https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
