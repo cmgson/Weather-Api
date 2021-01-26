0e278952d6545a3f46c0f5fb266991408234d7c3
Chris Gutierrez <cmgson1@gmail.com>
2021-01-26 15:52:38 -0600

    just about there, just having trouble with moment

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 $latRelay = "";
 $lonRelay = "";
 $grabCity = "";
-$forecastTemp = $('.forecastTemp');
+// $currentDate = ('#currentDate');
+$todaysDate = moment().format('LL');
+('#currentDate').text($todaysDate);
 
-// $city = "minneapolis";
 $apiKey = "9ec8fa29518e4540182f9fb78ea5599d";
 // $queryUrl =
 //   "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -18,11 +19,13 @@ $paintUVI = $("#index");
 $fiveDay = $(".fiveDay");
 $searchBtn = $("#searchBtn");
 $searchField = $("#searchField");
+$weatherReport = $('#weatherReport');
 // $grabCity = $('#searchField').val();
 // console.log($grabCity);
 // console.log($queryUrl);
 // console.log($fiveDay);
-// $fiveDay.hide();
+$fiveDay.hide();
+$weatherReport.hide();
 $searchBtn.on("click", function () {
   event.preventDefault();
   $grabCity = $("#searchField").val();
@@ -62,19 +65,86 @@ $searchBtn.on("click", function () {
       $paintHumid.text("Humidity: " + response.current.humidity + " %");
       $paintWind.text("Wind Speed: " + response.current.wind_speed + " MPH");
       $paintUVI.text("UV Index: " + response.current.uvi);
+
+      $dates = [];
+      $dailyIcons = [];
+      $dailyTemp = [];
+      $dailyHumidity = [];
+      $forecastIcons = $('.icons');
+      $forecastTemp = $('.forecastTemp');
+      $forecastHumidty = $('.forecastHumidity');
+      $forecastDate = $('.forecastDate');
+      var iconUrl = 'http://openweathermap.org/img/wn/';
+
+      for (var i = 0; i < 5; i++) {
+        $saveTemp = response.daily[i].temp.day;
+        $dailyTemp.push($saveTemp);
+      };
+
+      console.log($dailyTemp);
+      $forecastTemp.each(function(index){
+        $(this).html('Temp : ' + $dailyTemp[index] + '&deg');
+      });
+
+      for (var i = 0; i < 5; i++) {
+        $saveIcons = response.daily[i].weather[0].icon;
+        $dailyIcons.push($saveIcons);
+
+      };
+      
+      console.log($dailyIcons);
+      $forecastIcons.each(function(index){
+        $(this).attr('src', iconUrl + $dailyIcons[index] + '.png');
+      });
+
+      for (var i = 0; i < 5; i++) {
+        $saveHumidity = response.daily[i].humidity;
+        $dailyHumidity.push($saveHumidity);
+      };
+
+      $forecastHumidty.each(function(index){
+        $(this).html('Humidity : ' + $dailyHumidity[index] + '&deg');
+      });
+
+      for (var i = 0; i < 5; i++) {
+        $saveDate = response.daily[i].dt;
+        $dateString = moment.unix($saveDate).format("MM/DD/YYYY");
+        $dates.push($dateString);
+      };
+      console.log($dates);
+
+      $forecastDate.each(function(index){
+        $(this).text($dates[index]);
+        
+      });
+      
+      
+      $fiveDay.show();
+      $weatherReport.show();
+
+
+
+
+
+
+
+      
+      
+      
+      
       // console.log(response.daily[0].temp.day);
       
-      $forecastTemp.each(function() {
+      // $forecastTemp.each(function() {
+      //   console.log($(this));
+      //   for (var i = 0; i < 5; i++) {
+      //     $dailytemp = ''
+      //     $dailytemp = response.daily[i].temp.day;
+      //     $(this).text($dailytemp);
+      //     console.log($dailytemp);
+      //     // console.log(response.daily[i].temp.day);
+      //   };  
         
-        for (var i = 0; i < 5; i++) {
-          $dailytemp = '';
-          $dailytemp = response.daily[i].temp.day;
-          $(this).text($dailytemp);
-          console.log($dailytemp);
-          // console.log(response.daily[i].temp.day);
-        };  
-        
-      }); 
+      // }); 
       // for (var i= 0; i<5; i++) {
       //   console.log($(this));
       //   $dailytemp = response.daily[i].temp.day;
@@ -83,4 +153,5 @@ $searchBtn.on("click", function () {
     
     });
   });
+  console.log(moment().format("MM/DD/YYYY"));
 });
