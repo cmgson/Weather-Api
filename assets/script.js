b05986958b79de7ad823f4a5bf1faa0bea17fd7f
Chris Gutierrez <cmgson1@gmail.com>
2021-01-26 00:08:13 -0600

    populating page with java

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,86 @@
 $latRelay = "";
 $lonRelay = "";
+$grabCity = "";
+$forecastTemp = $('.forecastTemp');
 
-$city = "minneapolis";
+// $city = "minneapolis";
 $apiKey = "9ec8fa29518e4540182f9fb78ea5599d";
-$queryUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  $city +
-  "&units=imperial&appid=" +
-  $apiKey;
+// $queryUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   $grabCity +
+//   "&units=imperial&appid=" +
+//   $apiKey;
 $paintCity = $("#city");
 $paintTemp = $("#temp");
 $paintHumid = $("#humidity");
 $paintWind = $("#wind");
 $paintUVI = $("#index");
 $fiveDay = $(".fiveDay");
-console.log($queryUrl);
-console.log($fiveDay);
-$fiveDay.hide();
-
-$.ajax({
-  url: $queryUrl,
-  method: "GET",
-}).then(function (response) {
-  $latRelay = response.coord.lat;
-  $lonRelay = response.coord.lon;
-  console.log($latRelay);
-  console.log($lonRelay);
-  console.log(response);
-  $oneCallUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    $latRelay +
-    "&lon=" +
-    $lonRelay +
-    "&units=imperial" +
-    "&appid=" +
+$searchBtn = $("#searchBtn");
+$searchField = $("#searchField");
+// $grabCity = $('#searchField').val();
+// console.log($grabCity);
+// console.log($queryUrl);
+// console.log($fiveDay);
+// $fiveDay.hide();
+$searchBtn.on("click", function () {
+  event.preventDefault();
+  $grabCity = $("#searchField").val();
+  console.log($grabCity);
+  $queryUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    $grabCity +
+    "&units=imperial&appid=" +
     $apiKey;
-  console.log(response.name);
-  $paintCity.text(response.name);
 
   $.ajax({
-    url: $oneCallUrl,
+    url: $queryUrl,
     method: "GET",
   }).then(function (response) {
+    $latRelay = response.coord.lat;
+    $lonRelay = response.coord.lon;
+    console.log($latRelay);
+    console.log($lonRelay);
     console.log(response);
-    $paintTemp.html("Temperature: " + response.current.temp + "&deg F");
-    $paintHumid.text("Humidity: " + response.current.humidity + " %");
-    $paintWind.text("Wind Speed: " + response.current.wind_speed + " MPH");
-    $paintUVI.text("UV Index: " + response.current.uvi);
-    console.log(response.daily[0].temp.day);
+    $oneCallUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      $latRelay +
+      "&lon=" +
+      $lonRelay +
+      "&units=imperial" +
+      "&appid=" +
+      $apiKey;
+    console.log(response.name);
+    $paintCity.text(response.name);
+
+    $.ajax({
+      url: $oneCallUrl,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      $paintTemp.html("Temperature: " + response.current.temp + "&deg F");
+      $paintHumid.text("Humidity: " + response.current.humidity + " %");
+      $paintWind.text("Wind Speed: " + response.current.wind_speed + " MPH");
+      $paintUVI.text("UV Index: " + response.current.uvi);
+      // console.log(response.daily[0].temp.day);
+      
+      $forecastTemp.each(function() {
+        
+        for (var i = 0; i < 5; i++) {
+          $dailytemp = '';
+          $dailytemp = response.daily[i].temp.day;
+          $(this).text($dailytemp);
+          console.log($dailytemp);
+          // console.log(response.daily[i].temp.day);
+        };  
+        
+      }); 
+      // for (var i= 0; i<5; i++) {
+      //   console.log($(this));
+      //   $dailytemp = response.daily[i].temp.day;
+      //   console.log($dailytemp);
+      //   $('.forecastTemp').each($dailytemp);
+    
+    });
   });
 });
