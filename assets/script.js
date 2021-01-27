5d3eb7191109dd8e2f478f42730709a862f93041
Chris Gutierrez <cmgson1@gmail.com>
2021-01-27 12:15:55 -0600

    working on memeory

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,13 @@
 $latRelay = "";
 $lonRelay = "";
 $grabCity = "";
-// $currentDate = ('#currentDate');
+
 $todaysDate = moment().format('LL');
-('#currentDate').text($todaysDate);
+$('#currentDate').text($todaysDate);
 
 $apiKey = "9ec8fa29518e4540182f9fb78ea5599d";
-// $queryUrl =
-//   "https://api.openweathermap.org/data/2.5/weather?q=" +
-//   $grabCity +
-//   "&units=imperial&appid=" +
-//   $apiKey;
+
+$buttons = $('.cityBtn');
 $paintCity = $("#city");
 $paintTemp = $("#temp");
 $paintHumid = $("#humidity");
@@ -20,138 +17,128 @@ $fiveDay = $(".fiveDay");
 $searchBtn = $("#searchBtn");
 $searchField = $("#searchField");
 $weatherReport = $('#weatherReport');
-// $grabCity = $('#searchField').val();
-// console.log($grabCity);
-// console.log($queryUrl);
-// console.log($fiveDay);
+$cityArray = [];
+// hide until populated
+
 $fiveDay.hide();
 $weatherReport.hide();
+console.log($searchField);
+//click event for search button that triggers the ajax routine
 $searchBtn.on("click", function () {
-  event.preventDefault();
-  $grabCity = $("#searchField").val();
-  console.log($grabCity);
-  $queryUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    $grabCity +
-    "&units=imperial&appid=" +
-    $apiKey;
-
-  $.ajax({
-    url: $queryUrl,
-    method: "GET",
-  }).then(function (response) {
-    $latRelay = response.coord.lat;
-    $lonRelay = response.coord.lon;
-    console.log($latRelay);
-    console.log($lonRelay);
-    console.log(response);
-    $oneCallUrl =
-      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-      $latRelay +
-      "&lon=" +
-      $lonRelay +
-      "&units=imperial" +
-      "&appid=" +
+  weatherDisplay();
+  function weatherDisplay() {
+    event.preventDefault();
+    
+    $grabCity = $("#searchField").val();
+    $cityArray.push($grabCity);
+    console.log($grabCity);
+    $createButton = $('<div>');
+    $createButton.addClass('divRender');
+    $createButton.attr('data-city', $grabCity);
+    
+    var p = $('<p>').text($grabCity);
+    $createButton.append(p);
+  
+    $('#results').prepend($createButton);
+    $queryUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      $grabCity +
+      "&units=imperial&appid=" +
       $apiKey;
-    console.log(response.name);
-    $paintCity.text(response.name);
 
     $.ajax({
-      url: $oneCallUrl,
+      url: $queryUrl,
       method: "GET",
     }).then(function (response) {
+      $latRelay = response.coord.lat;
+      $lonRelay = response.coord.lon;
+      console.log($latRelay);
+      console.log($lonRelay);
       console.log(response);
-      $paintTemp.html("Temperature: " + response.current.temp + "&deg F");
-      $paintHumid.text("Humidity: " + response.current.humidity + " %");
-      $paintWind.text("Wind Speed: " + response.current.wind_speed + " MPH");
-      $paintUVI.text("UV Index: " + response.current.uvi);
+      $oneCallUrl =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        $latRelay +
+        "&lon=" +
+        $lonRelay +
+        "&units=imperial" +
+        "&appid=" +
+        $apiKey;
+      console.log(response.name);
+      $paintCity.text(response.name);
 
-      $dates = [];
-      $dailyIcons = [];
-      $dailyTemp = [];
-      $dailyHumidity = [];
-      $forecastIcons = $('.icons');
-      $forecastTemp = $('.forecastTemp');
-      $forecastHumidty = $('.forecastHumidity');
-      $forecastDate = $('.forecastDate');
-      var iconUrl = 'http://openweathermap.org/img/wn/';
+      $.ajax({
+        url: $oneCallUrl,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        $paintTemp.html("Temperature: " + response.current.temp + "&deg F");
+        $paintHumid.text("Humidity: " + response.current.humidity + " %");
+        $paintWind.text("Wind Speed: " + response.current.wind_speed + " MPH");
+        $paintUVI.text("UV Index: " + response.current.uvi);
 
-      for (var i = 0; i < 5; i++) {
-        $saveTemp = response.daily[i].temp.day;
-        $dailyTemp.push($saveTemp);
-      };
+        //set up arrays for all data from the response
+        $dates = [];
+        $dailyIcons = [];
+        $dailyTemp = [];
+        $dailyHumidity = [];
+        $forecastIcons = $('.icons');
+        $forecastTemp = $('.forecastTemp');
+        $forecastHumidty = $('.forecastHumidity');
+        $forecastDate = $('.forecastDate');
+        var iconUrl = 'https://openweathermap.org/img/wn/';
 
-      console.log($dailyTemp);
-      $forecastTemp.each(function(index){
-        $(this).html('Temp : ' + $dailyTemp[index] + '&deg');
-      });
+        //for loops to populate arrays
+        for (var i = 0; i < 5; i++) {
+          $saveTemp = response.daily[i].temp.day;
+          $dailyTemp.push($saveTemp);
+        };
 
-      for (var i = 0; i < 5; i++) {
-        $saveIcons = response.daily[i].weather[0].icon;
-        $dailyIcons.push($saveIcons);
+        console.log($dailyTemp);
+        $forecastTemp.each(function(index){
+          $(this).html('Temp : ' + $dailyTemp[index] + '&deg');
+        });
 
-      };
+        for (var i = 0; i < 5; i++) {
+          $saveIcons = response.daily[i].weather[0].icon;
+          $dailyIcons.push($saveIcons);
+
+        };
       
-      console.log($dailyIcons);
-      $forecastIcons.each(function(index){
-        $(this).attr('src', iconUrl + $dailyIcons[index] + '.png');
-      });
+        console.log($dailyIcons);
+        $forecastIcons.each(function(index){
+          $(this).attr('src', iconUrl + $dailyIcons[index] + '.png');
+        });
 
-      for (var i = 0; i < 5; i++) {
-        $saveHumidity = response.daily[i].humidity;
-        $dailyHumidity.push($saveHumidity);
-      };
+        for (var i = 0; i < 5; i++) {
+          $saveHumidity = response.daily[i].humidity;
+          $dailyHumidity.push($saveHumidity);
+        };
 
-      $forecastHumidty.each(function(index){
-        $(this).html('Humidity : ' + $dailyHumidity[index] + '&deg');
-      });
+        $forecastHumidty.each(function(index){
+          $(this).html('Humidity : ' + $dailyHumidity[index] + '&deg');
+        });
 
-      for (var i = 0; i < 5; i++) {
-        $saveDate = response.daily[i].dt;
-        $dateString = moment.unix($saveDate).format("MM/DD/YYYY");
-        $dates.push($dateString);
-      };
-      console.log($dates);
+        for (var i = 0; i < 5; i++) {
+          $saveDate = response.daily[i].dt;
+          $dateString = moment.unix($saveDate).format("MM/DD/YYYY");
+          $dates.push($dateString);
+        };
+        console.log($dates);
 
-      $forecastDate.each(function(index){
-        $(this).text($dates[index]);
+        $forecastDate.each(function(index){
+          $(this).text($dates[index]);
         
+        });
+      
+      
+        $fiveDay.show();
+        $weatherReport.show();
       });
-      
-      
-      $fiveDay.show();
-      $weatherReport.show();
-
-
-
-
-
-
-
-      
-      
-      
-      
-      // console.log(response.daily[0].temp.day);
-      
-      // $forecastTemp.each(function() {
-      //   console.log($(this));
-      //   for (var i = 0; i < 5; i++) {
-      //     $dailytemp = ''
-      //     $dailytemp = response.daily[i].temp.day;
-      //     $(this).text($dailytemp);
-      //     console.log($dailytemp);
-      //     // console.log(response.daily[i].temp.day);
-      //   };  
-        
-      // }); 
-      // for (var i= 0; i<5; i++) {
-      //   console.log($(this));
-      //   $dailytemp = response.daily[i].temp.day;
-      //   console.log($dailytemp);
-      //   $('.forecastTemp').each($dailytemp);
     
     });
-  });
-  console.log(moment().format("MM/DD/YYYY"));
+    console.log(moment().format("MM/DD/YYYY"));
+   
+   console.log('this is the city array---' + $cityArray);
+    localStorage.setItem('cities', JSON.stringify($cityArray));
+  };
 });
